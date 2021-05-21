a5c1a0b3c9fa655ef55cab00f0a615a61092ea39
Alessio <alessio.didio530@edu.unito.it>
2021-05-21 14:51:43 +0200

    SimpCoin elements

--- dapp/src/constants.js
+++ dapp/src/constants.js
@@ -1,7 +1,10 @@
 import MyERC20Token from './abi/MyERC20Token.json'
 import CryoCoin from './abi/CryoCoin.json';
+import SimpCoin from './abi/SimpCoin.json';
 
 export const bubaContractAddress = "0x5E68826aBf90B23a7757657f3138f2a1AAF21174";
 export const cryoContractAddress = "0x9fae9c0594b56b53cfb1a9b03b2747404840400e";
+export const simpContractAddress = "0xf7a1B265A952Bc7b6aA5720798E21C98f53de10F";
 export const bubaContractAbi = MyERC20Token.abi;
 export const cryoContractAbi = CryoCoin.abi;
+export const simpContractAbi = SimpCoin.abi;
--- dapp/src/index.js
+++ dapp/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import {bubaContractAbi, bubaContractAddress, cryoContractAddress, cryoContractAbi} from './constants';
+import {bubaContractAbi, bubaContractAddress, cryoContractAddress, cryoContractAbi,simpContractAddress,simpContractAbi} from './constants';
 import './constants';
 import React from 'react';
 import Contract from './Contract';
@@ -8,11 +8,13 @@ class ButtonToToken extends React.Component {
   render() {
     const bubaContractInstance = new Contract(bubaContractAbi, bubaContractAddress);
     const cryoContractInstance = new Contract(cryoContractAbi, cryoContractAddress);
+    const simpContractInstance = new Contract(simpContractAbi, simpContractAddress);
     console.log(bubaContractInstance.getNameOfToken());
     return (
       <div style={{ textAlign: 'center', paddingTop: '20%' }}>
         <button onClick={async () => bubaContractInstance.getNameOfToken()}>Buba</button>
         <button onClick={async () => cryoContractInstance.getNameOfToken()}>Cryo</button>
+        <button onClick={async () => simpContractInstance.getNameOfToken()}>Simp</button>
       </div>
     )
   }
